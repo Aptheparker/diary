7c9c48f4da9e8c90f42c2b73880fb55dfeecad2b
aptheparker <aptheparker@gmail.com>
2023-08-06 16:23:47 +0800

    buttons cursor and size updated

--- src/pages/ContentPage/ContentPage.js
+++ src/pages/ContentPage/ContentPage.js
@@ -1,5 +1,4 @@
-// css
-import { useState } from "react";
+import React, { useState } from "react";
 import classes from "./ContentPage.module.css";
 
 const ContentPage = ({ contentImageRef }) => {
